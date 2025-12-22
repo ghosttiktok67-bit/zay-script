6d88855d45bdbe2b78ecfd9ffc9cc861963c4f19
ghosttiktok67-bit <ghosttiktok67@gmail.com>
2025-12-22 15:40:06 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -24,7 +24,7 @@ local playerGui = player:WaitForChild("PlayerGui")
 local Whitelist = {
     9173033891, -- moi
     5182071786, -- Ami 1
-    1234567890, -- Ami 2
+    5125152468, -- ACHETEUR KETUPAT 105M
     9876543210, -- Ami 3
     1122334455, -- Ami 4
     5566778899, -- Ami 5
