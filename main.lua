16bc8de6288f8d84cbead2414b0637ff8bccf8fe
luna <ghosttiktok67@gmail.com>
2025-12-28 21:47:39 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -37,7 +37,7 @@ local Whitelist = {
     4269265728, -- acheteur 10euro
     1617845814, -- free le bon 13
     1234454555, -- ACHETEUR KEBECOIS
-    10133764831,-- mec a enlever bientot
+    3212312321,-- Ami 
     9212405626, -- tic tac couz a 13
     0, -- Place libre 2
     0, -- Place libre 3
